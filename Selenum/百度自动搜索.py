00f5c4dd03dcd67e6a5a2bc559330eeb3e080bf4
mack98 <997891390@qq.com>
2020-12-18 19:47:04 +0800

    提交百度搜索 第一部分

--- Selenum/百度自动搜索.py
+++ Selenum/百度自动搜索.py
@@ -1,1 +1,12 @@
-rrr v
+from selenium import webdriver
+
+url = "https://www.baidu.com"
+
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+input_element = driver.find_element_by_id()
+click_element = driver.find_element_by_id()
+
+click_element.click()
